8d8e1df3c3e6c5239f4ee49f8fc2d61c233fc0de
sachin <sachin.yadav@appinventiv.com>
2022-01-15 23:15:45 +0530

    minor changes

--- app/src/main/java/com/example/visahub/Otp.kt
+++ app/src/main/java/com/example/visahub/Otp.kt
@@ -1,11 +1,17 @@
 package com.example.visahub
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_otp.*
 
 class Otp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_otp)
+
+        login_btn.setOnClickListener {
+            startActivity(Intent(this, DashboardActivity::class.java))
+        }
     }
 }
