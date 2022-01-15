580792797a0d7dc2fdd9ee9158293bc93e646326
sachin <sachin.yadav@appinventiv.com>
2022-01-15 22:51:47 +0530

    intent added

--- app/src/main/java/com/example/visahub/Login.kt
+++ app/src/main/java/com/example/visahub/Login.kt
@@ -1,11 +1,17 @@
 package com.example.visahub
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        login_btn.setOnClickListener {
+            startActivity(Intent(this, Otp::class.java))
+        }
     }
 }
