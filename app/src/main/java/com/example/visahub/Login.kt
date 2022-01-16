5db0270495bd8a23c762d564e749f23bd7888b24
aurangzeb07 <aurang772000@gmail.com>
2022-01-16 18:45:48 +0530

    Converted To Fragment

--- app/src/main/java/com/example/visahub/Login.kt
+++ app/src/main/java/com/example/visahub/Login.kt
@@ -3,15 +3,23 @@ package com.example.visahub
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import kotlinx.android.synthetic.main.activity_login.*
 
 class Login : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        login_btn.setOnClickListener {
-            startActivity(Intent(this, Otp::class.java))
-        }
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
+        navHostFragment.navController
+//        setupActionBarWithNavController(navController)
     }
+
+
 }
--- app/src/main/java/com/example/visahub/MainActivity.kt
+++ app/src/main/java/com/example/visahub/MainActivity.kt
@@ -9,39 +9,9 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var authenticationViewModel: AuthenticationViewModel
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        authenticationViewModel = ViewModelProvider(this).get(AuthenticationViewModel::class.java)
-        continue_btn.setOnClickListener {
-            navigateToOtpScreen(
-                name_edit_text.text.toString(),
-                phone_edit_text.text.toString(),
-                email_edit_text.text.toString()
-            )
-        }
-
-        google_btn.setOnClickListener {
-            googleSignIn()
-        }
     }
 
-    private fun googleSignIn(){
-        startActivityForResult(authenticationViewModel.googleSignInClient(this).signInIntent, 200)
-    }
-
-    private fun navigateToOtpScreen(name: String, phone: String, email: String){
-        if (!name.isNullOrEmpty() && !phone.isNullOrEmpty() && !email.isNullOrEmpty()){
-            startActivity(Intent(this, Otp::class.java).apply {
-                putExtra("name", name)
-                putExtra("phone", phone)
-                putExtra("email", email)
-            })
-        } else {
-            Toast.makeText(this,"Please fill all 3 fields properly", Toast.LENGTH_SHORT).show()
-        }
-
-    }
 }
